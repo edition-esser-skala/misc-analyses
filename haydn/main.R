7d828fdc2229d4902462e1426a9da7b9f0c23e4b
Wolfgang Esser-Skala <wolfgang@esser-skala.at>
2022-10-21 08:17:45 +0200

    combines scripts

--- haydn/main.R
+++ haydn/main.R
@@ -1,9 +1,43 @@
 library(tidyverse)
 library(readODS)
+library(openxlsx)
 
 
 
-# Load data ---------------------------------------------------------------
+# Functions ---------------------------------------------------------------
+
+save_table <- function(tables, filename, sheet_name = "Sheet1") {
+  filename <- str_glue("{filename}.xlsx")
+  wb <- createWorkbook()
+
+  # ensure that tables is a named list
+  if (inherits(tables, "list")) {
+    if (is.null(names(tables)))
+      tables <- set_names(tables, paste0("Sheet", seq_along(tables)))
+  } else if (inherits(tables, "data.frame")) {
+    tables <- list(tables) %>% set_names(sheet_name)
+  } else {
+    stop("'tables' must be a data frame or list of data frames.")
+  }
+
+  # populate Excel file with worksheets
+  iwalk(
+    tables,
+    function(table, sheet_name) {
+      addWorksheet(wb, sheet_name)
+      writeData(
+        wb,
+        sheet_name,
+        table,
+        headerStyle = createStyle(textDecoration = "bold")
+      )
+      freezePane(wb, sheet_name, firstRow = TRUE)
+      setColWidths(wb, sheet_name, 1:ncol(table), "auto")
+    }
+  )
+
+  saveWorkbook(wb, filename, overwrite = TRUE)
+}
 
 load_works <- function(file, siglum, ignored_mh = c("???", "deest")) {
   read_ods(file, col_types = cols(.default = "c")) %>%
@@ -18,12 +52,15 @@ load_works <- function(file, siglum, ignored_mh = c("???", "deest")) {
 }
 
 
+
+# Load data ---------------------------------------------------------------
+
 mh <-
   read_ods("data/mh.ods") %>%
   as_tibble() %>%
   rename(manuscript_source = source)
 
-works <-
+digitized_works <-
   tribble(
     ~file,                   ~siglum,
     "data/works A-Ed.ods",   "A-Ed",
@@ -40,8 +77,12 @@ works <-
   ) %>%
   pmap_dfr(load_works)
 
-available_works <-
-  works %>%
+
+
+# Digitized works to be edited --------------------------------------------
+
+dig_works_tbe <-
+  digitized_works %>%
   group_by(mh) %>%
   summarise(
     digital_source = str_c(shelfmark, collapse = "; "),
@@ -50,10 +91,7 @@ available_works <-
              else stop("Inconsistency detected")
   )
 
-works %>% filter(mh == "525")
-
-
-# Save results ------------------------------------------------------------
+# dig_works_tbe %>% filter(mh == "635")
 
 used_genres <- c(
   "antiphon", "completorium", "gradual", "hymn", "introitus", "motet",
@@ -63,7 +101,7 @@ used_genres <- c(
 ignored_notes <- c("fragment", "lost", "incomplete")
 
 mh %>%
-  left_join(available_works, by = "mh") %>%
+  left_join(dig_works_tbe, by = "mh") %>%
   mutate(
     notes = notes %>% replace_na(""),
     manuscript_source = manuscript_source %>% replace_na(""),
@@ -72,5 +110,37 @@ mh %>%
   ) %>%
   filter(genre %in% used_genres, !notes %in% ignored_notes) %>%
   relocate(digital_source, .after = genre) %>%
-  write_ods("output/all_works.ods")
+  write_ods("output/all_works_to_be_edited.ods")
 
+
+
+# Works by library --------------------------------------------------------
+
+manuscript_works <-
+  mh %>%
+  left_join(dig_works_tbe, by = "mh") %>%
+  filter(!is.na(manuscript_source)) %>%
+  mutate(manuscript_source = str_split(manuscript_source, ", *")) %>%
+  unnest_longer(manuscript_source) %>%
+  mutate(
+    manuscript_source =
+      manuscript_source %>%
+      str_match("([^\\s]+)([^\\(]+)?(.+)?") %>%
+      as_tibble(.name_repair = "minimal") %>%
+      magrittr::set_colnames(c("match", "siglum", "shelfmark", "rism"))
+  ) %>%
+  unpack(manuscript_source) %>%
+  mutate(
+    shelfmark = str_trim(shelfmark),
+    rism = str_extract(rism, "\\d+")
+  )
+
+
+manuscript_works %>%
+  count(siglum)
+
+manuscript_works %>%
+  mutate(digitally_available = if_else(is.na(digital_source), "", "yes")) %>%
+  select(!c(match, digital_source, edited)) %>%
+  split(.$siglum) %>%
+  save_table("output/works_by_library")
